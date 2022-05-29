4b9ce4cb5c53189a0d4bee40b9bcb7a8d9f47985
Alexey <lxmlshv97@mail.ru>
2022-05-29 22:44:26 +0300

    fix config gulp

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -10,36 +10,36 @@ import groupCssMediaQueries from 'gulp-group-css-media-queries'; // Групир
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
-	return app.gulp.src(app.path.src.scss, { sourcemaps: true })
-	.pipe(app.plugins.plumber(
-		app.plugins.notify.onError({
-			title: "SCSS",
-			message: "Error: <%= error.message %>"
-		})))
-	.pipe(app.plugins.replace(/@img\//g, '../img/'))
-	.pipe(sass({
-		outputStyle: 'expanded'
-	}))
-	.pipe(groupCssMediaQueries())
-	.pipe(
-		app.plugins.if(
-			app.isBuild,
-			autoprefixer({
-				grid: true,
-				overrideBrowserslist: ["last 3 versions"],
-				cascade: true
-			})
+	return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
+		.pipe(app.plugins.plumber(
+			app.plugins.notify.onError({
+				title: "SCSS",
+				message: "Error: <%= error.message %>"
+			})))
+		.pipe(app.plugins.replace(/@img\//g, '../img/'))
+		.pipe(sass({
+			outputStyle: 'expanded'
+		}))
+		.pipe(groupCssMediaQueries())
+		.pipe(
+			app.plugins.if(
+				app.isBuild,
+				autoprefixer({
+					grid: true,
+					overrideBrowserslist: ["last 3 versions"],
+					cascade: true
+				})
 			)
 		)
-	.pipe(
-		app.plugins.if(
-			app.isBuild,
-			webpcss(
-			{
-				webpClass: ".webp",
-				noWebpClass: ".no-webp"
-			}
-			)
+		.pipe(
+			app.plugins.if(
+				app.isBuild,
+				webpcss(
+					{
+						webpClass: ".webp",
+						noWebpClass: ".no-webp"
+					}
+				)
 			)
 		)
 		// Раскомментировать если нужен не сжатый дубль файла стилей
@@ -48,12 +48,12 @@ export const scss = () => {
 			app.plugins.if(
 				app.isBuild,
 				cleanCss()
-				)
 			)
+		)
 		.pipe(rename({
 			extname: ".min.css"
 		}))
 		.pipe(app.gulp.dest(app.path.build.css))
 		.pipe(app.plugins.browsersync.stream());
-	}
+}
 
