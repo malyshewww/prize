a738294db365b1fd5852bbc1113f52243e4d4d53
Alexey <malyshev_ag@komplex-group.ru>
2022-06-06 10:17:51 +0300

    Исправлен dropdown в категориях, добавлен скрипт для стрелок перед submenu, работа с изображениями в разделе Состав в Карточке товара, в превью товара

--- src/js/app.js
+++ src/js/app.js
@@ -53,6 +53,20 @@ function addTouchClass() {
 }
 addTouchClass();
 
+// Добавление стрелок, если имеется submenu
+let menuArrows = document.querySelectorAll('.menu__arrow');
+if (menuArrows.length > 0) {
+	for (let index = 0; index < menuArrows.length; index++) {
+		let menuArrow = menuArrows[index];
+		const submenu = menuArrow.nextElementSibling;
+		if (submenu != null) {
+			menuArrow.style.display = 'block';
+		} else {
+			menuArrow.style.display = 'none';
+		}
+	}
+}
+
 // Подключаем Галерею LightGallery
 import './libs/lightgallery.min.js';
 
