414abccff986160e969b0d9749fceeed6332cfc1
Alexey <malyshev_ag@komplex-group.ru>
2022-07-27 18:24:32 +0300

    work with select-group in preview product

--- app/scripts/modules/choice.js
+++ app/scripts/modules/choice.js
@@ -1,38 +1,70 @@
 // Переключение кнопок с превью карточки товара
 const choiceGroup = document.querySelectorAll('[data-select]');
 choiceGroup.forEach((item, i, arr) => {
+   const choiceItemHeader = item.querySelector('.select-group__item');
+   const choiceItem = item.querySelector('.select-group__choice');
    const choiceList = item.querySelector('.dropdown-list');
    const choiceArrow = item.querySelector('.select-group__arrow');
-   const choiceGroupLabels = item.querySelectorAll('.dropdown-list .select-group');
+   const choiceDropDownItems = item.querySelectorAll('.dropdown-list .select-group__item');
 
    const choiceValue = item.querySelector('[data-select-value]');
    const inputHidden = item.querySelector('.select-group__input-hidden');
+
+
+
+   const product = item.closest('.card-product');
+   const productPrice = product.querySelector('.card-product__price');
+
    if (choiceList) {
-      item.addEventListener('click', (e) => {
+      choiceItem.addEventListener('click', (e) => {
          e.stopPropagation();
          arr.forEach(arrElement => arrElement.classList.toggle('active', arrElement === item))
       });
       choiceArrow.classList.add('show');
    }
-   choiceGroupLabels.forEach((label) => {
-      label.addEventListener('click', (e) => {
-         const choiceDropdownValue = label.querySelector('[data-select-dropdown-value]');
+
+   choiceItemHeader.addEventListener('click', (e) => {
+      const choiceItemHeaderPrice = choiceItemHeader.dataset.price;
+      productPrice.innerText = choiceItemHeaderPrice + " ₽";
+   })
+
+   choiceDropDownItems.forEach((dropdownItem) => {
+      const choiceDropdownValue = dropdownItem.querySelector('[data-select-dropdown-value]');
+      dropdownItem.addEventListener('click', (e) => {
          e.stopPropagation();
+         const realInput = item.querySelector('.real-radio').checked = true;
+         const priceValue = dropdownItem.dataset.price;
+         productPrice.innerText = priceValue + " ₽";
+
          choiceValue.innerText = choiceDropdownValue.innerText;
          inputHidden.value = choiceDropdownValue.innerText.replace(/[a-zа-яё]/gi, '');
          item.classList.remove('active');
+         if (choiceDropdownValue.innerText == choiceValue.innerText) {
+            dropdownItem.style.display = "none";
+         }
+         if (choiceDropdownValue.innerText !== choiceValue.innerText) {
+            dropdownItem.style.display = "flex";
+         } else {
+            return;
+         }
       })
    })
-   // Клик снаружи [data-choice-wrap]. Закрыть [data-select]
-   document.addEventListener('click', function (e) {
+});
+
+
+// Клик снаружи [data-select]. Закрыть [data-select]
+document.addEventListener('click', function (e) {
+   choiceGroup.forEach((item) => {
       if (e.target != item) {
          item.classList.remove('active')
       }
    })
-   // Нажатие на Tab или Escape. Закрыть [data-select]
-   document.addEventListener('keydown', function (e) {
+})
+// Нажатие на Tab или Escape. Закрыть [data-select]
+document.addEventListener('keydown', function (e) {
+   choiceGroup.forEach((item) => {
       if (e.key === 'Tab' || e.key === 'Escape') {
          item.classList.remove('active')
       }
-   });
+   })
 });
