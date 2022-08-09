61a04626d44da2272ac667ddc8b3c6d8281b3ed6
Alexey <malyshev_ag@komplex-group.ru>
2022-08-09 17:45:41 +0300

    work with modals window

--- app/scripts/modules/forms.js
+++ app/scripts/modules/forms.js
@@ -18,4 +18,225 @@ fields.forEach(input => {
    input.addEventListener("blur", remfocus)
 })
 
-import "./maskphone.js";
+// скрытый input
+const workemailList = document.querySelectorAll('.workemail');
+workemailList.forEach((item) => {
+   item.value = '';
+   item.removeAttribute('required');
+});
+// Маска для телефона
+(function () {
+   function mask(phone) {
+      var code = '+7',
+         find = /\+7/;
+      code = '+';
+      find = /\+/;
+      phone.addEventListener('focus', function () {
+         phone.value = code + " " + phone.value.replace(code + ' ', '');
+      });
+      phone.addEventListener('input', function () {
+         var val = phone.value.replace(find, ''),
+            res = code + " ";
+         val = val.replace(/[^0-9]/g, '');
+
+         for (var i = 0; i < val.length; i++) {
+            //res+= i===0?' ':'';
+            res += i === 1 ? ' (' : '';
+            res += i == 4 ? ') ' : '';
+            res += i == 7 || i == 9 ? ' ' : '';
+            if (i == 11) break;
+            res += val[i];
+         }
+
+         phone.value = res;
+      });
+      phone.addEventListener('blur', function () {
+         var val = phone.value.replace(find, '');
+         val = val.trim();
+         if (!val) phone.value = null;
+      });
+   }
+   var phone = document.querySelectorAll('input[name="phone"]');
+   if (phone) {
+      var i = phone.length;
+      while (i--) {
+         mask(phone[i]);
+      }
+   }
+   return true;
+})();
+
+// Подключение библиотеки для модальных окон
+import "./graph-modal.min.js";
+const modals = new GraphModal();
+
+// Отправка формы
+const modalForm = document.querySelectorAll('.form');
+const closeModalBtn = document.querySelector('.js-modal-close');
+const formAllInputs = document.querySelectorAll('.form__input, .form__textarea, .form-modal__input, .form-modal__textarea');
+
+function formHandler(formId, path) {
+   const formElement = document.getElementById(formId);
+   if (formElement) {
+      formElement.addEventListener('submit', (event) => {
+         event.preventDefault();
+         const thisForm = event.target;
+         const formData = new FormData(thisForm);
+         // кнопка "Отправить"
+         const buttonSubmit = thisForm.querySelector('.form__button');
+
+         const buttonSubmitText = buttonSubmit.textContent;
+         buttonSubmit.setAttribute('disabled', 'true');
+         buttonSubmit.classList.add('disabled')
+         buttonSubmit.textContent = 'идет отправка...';
+         fetch(path, {
+            method: 'POST',
+            body: formData,
+         })
+            // "status" в MODX
+            .then((response) => response.json())
+            .then((result) => {
+               const inputName = thisForm.querySelector('input[name="name"]');
+               const inputPhone = thisForm.querySelector('input[name="phone"]');
+               const inputEmail = thisForm.querySelector('input[name="email"]');
+               const inputMessage = thisForm.querySelector('textarea[name="message"]');
+               if (result.status == "success") {
+                  // Вызывыаем модальку об успехе
+                  const modal = document.getElementById('notice');
+                  modal.classList.add('open-modal')
+                  setTimeout(() => {
+                     modal.classList.remove("open-modal");
+                  }, 5000)
+                  if (inputName) {
+                     inputName.value = '';
+                  }
+                  if (inputPhone) {
+                     inputPhone.value = '';
+                  }
+                  if (inputEmail) {
+                     inputEmail.value = '';
+                  }
+                  if (inputMessage) {
+                     inputMessage.value = '';
+                  }
+               } else {
+                  if (result.name) {
+                     inputName.classList.add('error');
+                     // inputName.setAttribute('title', result.name.trim());
+                  }
+                  if (result.phone) {
+                     inputPhone.classList.add('error');
+                     // inputPhone.setAttribute('title', result.phone.trim());
+                  }
+                  if (result.email) {
+                     inputEmail.classList.add('error');
+                     // inputEmail.setAttribute('title', result.email.trim());
+                  }
+                  if (result.message) {
+                     // inputMessage.setAttribute('title', result.message.trim());
+                  }
+               }
+               buttonSubmit.removeAttribute('disabled');
+               buttonSubmit.classList.remove('disabled');
+               buttonSubmit.textContent = buttonSubmitText;
+            }).catch((error) => {
+               const modal = document.getElementById('notice');
+               const notice = modal.querySelector('.modal__body');
+               notice.textContent = 'При отправке произошла ошибка, попробуйте еще раз';
+               buttonSubmit.removeAttribute('disabled');
+               buttonSubmit.classList.remove('disabled');
+               buttonSubmit.textContent = buttonSubmitText;
+            });
+      })
+   }
+}
+function formHandlerModal(formId, path) {
+   const formElement = document.getElementById(formId);
+   if (formElement) {
+      formElement.addEventListener('submit', (event) => {
+         event.preventDefault();
+         const thisForm = event.target;
+         const formData = new FormData(thisForm);
+         // кнопка "Отправить"
+         const buttonSubmit = thisForm.querySelector('.form-modal__button');
+
+         const buttonSubmitText = buttonSubmit.textContent;
+         buttonSubmit.setAttribute('disabled', 'true');
+         buttonSubmit.classList.add('disabled')
+         buttonSubmit.textContent = 'идет отправка...';
+         fetch(path, {
+            method: 'POST',
+            body: formData,
+         })
+            // "status" в MODX
+            .then((response) => response.json())
+            .then((result) => {
+               const inputName = thisForm.querySelector('input[name="name"]');
+               const inputPhone = thisForm.querySelector('input[name="phone"]');
+               const inputEmail = thisForm.querySelector('input[name="email"]');
+               const inputMessage = thisForm.querySelector('textarea[name="message"]');
+               if (result.status == "success") {
+                  // Вызывыаем модальку об успехе
+                  const modals = document.querySelectorAll('[data-modal]');
+                  modals.forEach((item) => {
+                     item.classList.remove('open-modal');
+                  })
+                  document.body.classList.remove('lock');
+                  const modal = document.getElementById('notice');
+                  modal.classList.add('open-modal');
+                  setTimeout(() => {
+                     modal.classList.remove("open-modal");
+                  }, 5000)
+                  if (inputName) {
+                     inputName.value = '';
+                  }
+                  if (inputPhone) {
+                     inputPhone.value = '';
+                  }
+                  if (inputEmail) {
+                     inputEmail.value = '';
+                  }
+                  if (inputMessage) {
+                     inputMessage.value = '';
+                  }
+               } else {
+                  if (result.name) {
+                     inputName.classList.add('error');
+                     // inputName.setAttribute('title', result.name.trim());
+                  }
+                  if (result.phone) {
+                     inputPhone.classList.add('error');
+                     // inputPhone.setAttribute('title', result.phone.trim());
+                  }
+                  if (result.email) {
+                     inputEmail.classList.add('error');
+                     // inputEmail.setAttribute('title', result.email.trim());
+                  }
+                  if (result.message) {
+                     // inputMessage.setAttribute('title', result.message.trim());
+                  }
+               }
+               buttonSubmit.removeAttribute('disabled');
+               buttonSubmit.classList.remove('disabled');
+               buttonSubmit.textContent = buttonSubmitText;
+            }).catch((error) => {
+               buttonSubmit.removeAttribute('disabled');
+               buttonSubmit.classList.remove('disabled');
+               buttonSubmit.textContent = buttonSubmitText;
+            });
+      })
+   }
+}
+formHandler("form", "/formhandler");
+formHandlerModal("form-buy", "/formhandlerbuy");
+
+formAllInputs.forEach((item) => {
+   const removeErrorClass = (event) => {
+      if (!event.target.classList.contains('error')) {
+         return false;
+      }
+      event.target.classList.remove('error');
+   };
+   item.addEventListener('input', removeErrorClass);
+   item.addEventListener('change', removeErrorClass);
+});
